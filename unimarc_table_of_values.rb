35e53feeb683194b72737ff41b084286f1373ad4
mansakondo <47113995+mansakondo@users.noreply.github.com>
2021-08-26 02:23:42 +0200

    Add files via upload

---
+++ unimarc_table_of_values.rb
@@ -0,0 +1,29 @@
+require "nokogiri"
+
+def fetch_unimarc_table_values_from_html
+  path = "unimarc_table_of_values.html"
+  html = File.open(path) { |f| Nokogiri::HTML(f) }
+
+  elements = html.xpath "//div/div"
+
+  table_starter = html.xpath("//div[normalize-space() = 'Field']").first
+  page_ender = html.xpath("//div[normalize-space() = '2008']").first
+
+  start_position = elements.index table_starter
+  end_position = elements.index page_ender
+
+  cells = elements[start_position...end_position]
+
+  rows = []
+  start = 0
+  while start < end_position
+    row = cells[start..(start + 5)]
+    rows << row unless row.nil?
+    start += 6
+  end
+
+  rows
+end
+
+require "pry"
+Pry.start
