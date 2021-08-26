6c5c2dff067b9472bcbe1cea9554de1d55a20237
mansakondo <47113995+mansakondo@users.noreply.github.com>
2021-08-26 13:50:56 +0200

    Add files via upload

--- unimarc-b-fieldlist.rb
+++ unimarc-b-fieldlist.rb
@@ -1,18 +1,41 @@
 require "nokogiri"
 require "csv"
+require "yaml"
 
 def fetch_unimarc_fieldlist_from_html(lang = :en)
   path = "unimarc-b-fieldlist-#{lang}.html"
   html = File.open(path) { |f| Nokogiri::HTML(f) }
 
   blocks = html.xpath "//div[@class='block']"
-  fields = html.xpath "//div[@class='block']/following-sibling::div[not(@class = 'block')]"
+  rows = html.xpath "//div[@class='block']/following-sibling::div[not(@class = 'block')]"
 
-  return blocks, fields
+  return blocks, rows
+end
+
+def format_rows(rows)
+  result = []
+
+  rows.each_slice(2) { |row| result << row.map(&:text) }
+
+  result
 end
 
 def convert_rows_to_csv(rows)
-  rows.map { |row| row.to_a.to_csv }.join
+  rows = format_rows rows
+  rows.map { |row| row.to_csv }.join
+end
+
+def convert_rows_to_yaml(rows, lang = nil)
+  rows = format_rows rows
+  hash = {}
+
+  if lang
+    hash[lang.to_sym] = rows.to_h
+  else
+    hash = rows.to_h
+  end
+
+  hash.to_yaml
 end
 
 require "pry"
