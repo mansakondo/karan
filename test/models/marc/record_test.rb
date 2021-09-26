8fc5058f13f8ced66f0fd9349ccd4848a531c43d
mansakondo <mansakondo22@gmail.com>
2021-09-26 17:22:31 +0200

    Add first test

--- test/models/marc/record_test.rb
+++ test/models/marc/record_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class MARC::RecordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures "marc/records"
+
+  setup do
+    @record = marc_records(:hamlet)
+  end
+
+  test "#repeated?" do
+    assert @record.repeated? "948"
+    assert @record["099"].repeated? "a"
+  end
 end
