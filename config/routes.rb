fe665a3948be5e19ede00e0f7c3ed627233c713d
mansakondo <mansakondo22@gmail.com>
2021-10-02 11:36:52 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :marc do
+    resources :records
+  end
   get 'hello/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
