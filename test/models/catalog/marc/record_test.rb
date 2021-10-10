c158b030394f4425ce8583851bb1a14daeaff98a
mansakondo <mansakondo22@gmail.com>
2021-10-10 19:55:18 +0200

    Test MARC decoding

--- test/models/catalog/marc/record_test.rb
+++ test/models/catalog/marc/record_test.rb
@@ -5,10 +5,21 @@ class Catalog::MARC::RecordTest < ActiveSupport::TestCase
 
   setup do
     @record = catalog_marc_records(:hamlet)
+
+    @conn = ZOOM::Connection.new.connect("catalog.brooklynpubliclibrary.org:210")
+    @conn.database_name = "innopac"
   end
 
   test "#repeated?" do
     assert @record.repeated? "948"
     assert @record["099"].repeated? "a"
+  end
+
+  test "should decode raw MARC data" do
+    results = @conn.search '@attr 1=4 "Sherlock Holmes"'
+    marc    = results[0].raw
+    record  = Catalog::MARC::Record.new_from_marc(marc)
+
+    assert record
   end
 end
