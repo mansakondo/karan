2e630f0994eba6a5111765f8322359ee3e98f8cf
mansakondo <mansakondo22@gmail.com>
2021-10-12 17:01:08 +0200

    Add `MARC::Record::Recordable`

---
+++ app/models/catalog/marc/record/recordable.rb
@@ -0,0 +1,20 @@
+module Catalog
+  module MARC::Record::Recordable
+    extend ActiveSupport::Concern
+
+    included do
+      has_one :marc_record,
+        class_name: "Catalog::MARC::Record",
+        as: :marc_recordable,
+        touch: true
+
+      delegate :format, to: :marc_record
+
+      class_eval <<-CODE, __FILE__, __LINE__ + 1
+        def mappings
+          MAPPINGS[format]
+        end
+      CODE
+    end
+  end
+end
