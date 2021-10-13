62f2094813fd2511d0e5faf7f0d8a33b20b64092
mansakondo <mansakondo22@gmail.com>
2021-10-13 19:51:06 +0200

    Refactor `read` method

--- app/models/catalog/marc/record/reading.rb
+++ app/models/catalog/marc/record/reading.rb
@@ -6,21 +6,21 @@ module Catalog
       def read(file, record_type: :bibliographic, format: "marc21", encoding: "UTF-8", autosave: false)
         reader = ::MARC::Reader.new(file, external_encoding: encoding)
 
+        case record_type
+        when :bibliographic
+          record_type_class = MARC::Record::BibliographicRecord
+        when :authority
+          record_type_class = MARC::Record::AuthorityRecord
+        end
+
         marc_recordables = []
 
         reader.each_raw do |raw|
-          record = new_from_marc(raw, encoding: encoding)
+          record          = new_from_marc(raw, encoding: encoding)
+          marc_recordable = record_type_class.new
 
-          record.format = format
-
-          case record_type
-          when :bibliographic
-            marc_recordable        = MARC::Record::BibliographicRecord.new
-            record.marc_recordable = marc_recordable
-          when :authority
-            marc_recordable        = MARC::Record::AuthorityRecord.new
-            record.marc_recordable = marc_recordable
-          end
+          record.format          = format
+          record.marc_recordable = marc_recordable
 
           if autosave == true
             record.fields.save
