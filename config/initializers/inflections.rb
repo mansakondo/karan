e99b90798c7b8c724955a78b7ff89af82403708f
mansakondo <mansakondo22@gmail.com>
2021-10-14 16:20:06 +0200

    Add `MARC21` and `UNIMARC` acronyms

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -13,4 +13,6 @@
 # These inflection rules are supported but not enabled by default:
 ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.acronym "MARC"
+  inflect.acronym "MARC21"
+  inflect.acronym "UNIMARC"
 end
