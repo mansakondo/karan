a13b10adace52d7cb10606d8c2c5cc04bdf287d9
mansakondo <mansakondo22@gmail.com>
2021-10-16 12:59:43 +0200

    Add `MARC::Processing` mixin

---
+++ app/models/catalog/marc/processing.rb
@@ -0,0 +1,22 @@
+module Catalog
+  module MARC::Processing
+    extend ActiveSupport::Concern
+
+    included do
+      include Processing
+
+      def identify(field)
+        field.tag
+      end
+    end
+
+    class_methods do
+      def process(record)
+        processor = new(record)
+        fields    = record.fields
+
+        processor.process_each(fields)
+      end
+    end
+  end
+end
