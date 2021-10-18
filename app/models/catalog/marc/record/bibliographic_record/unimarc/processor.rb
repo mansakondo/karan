31542edf73b82e83bb341b192551ceafb93cdcda
mansakondo <mansakondo22@gmail.com>
2021-10-18 21:39:56 +0200

    Resolve links in 7XX fields

--- app/models/catalog/marc/record/bibliographic_record/unimarc/processor.rb
+++ app/models/catalog/marc/record/bibliographic_record/unimarc/processor.rb
@@ -58,6 +58,32 @@ module Catalog
       alias on_675 on_6xx
       alias on_676 on_6xx
       alias on_680 on_6xx
+
+      def on_7xx(field)
+        targets = resolve_links_in field
+
+        targets.each do |target|
+          contributors = record.contributors
+
+          contributors << target unless contributors.include? target
+        end
+
+        field.as_json
+      end
+
+      alias on_700 on_7xx
+      alias on_701 on_7xx
+      alias on_702 on_7xx
+      alias on_710 on_7xx
+      alias on_711 on_7xx
+      alias on_712 on_7xx
+      alias on_713 on_7xx
+      alias on_716 on_7xx
+      alias on_720 on_7xx
+      alias on_721 on_7xx
+      alias on_722 on_7xx
+      alias on_723 on_7xx
+      alias on_730 on_7xx
     end
   end
 end
