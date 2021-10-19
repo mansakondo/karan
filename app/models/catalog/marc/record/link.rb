867f22cc5186f51a0f42903e4a46bb92ca3697e9
mansakondo <mansakondo22@gmail.com>
2021-10-19 11:35:38 +0200

    Add `:linkable` association

--- app/models/catalog/marc/record/link.rb
+++ app/models/catalog/marc/record/link.rb
@@ -1,5 +1,10 @@
 module Catalog
   class MARC::Record::Link < ApplicationRecord
+    TYPES = %w(
+      Catalog::MARC::Record::Link::Subject
+      Catalog::MARC::Record::Link::Contribution
+    ).freeze
+
     belongs_to :child,
       class_name: "Catalog::MARC::Record",
       inverse_of: :dependencies
@@ -7,5 +12,9 @@ module Catalog
     belongs_to :parent,
       class_name: "Catalog::MARC::Record",
       inverse_of: :references
+
+    delegated_type :linkable,
+      types: TYPES,
+      inverse_of: :link
   end
 end
