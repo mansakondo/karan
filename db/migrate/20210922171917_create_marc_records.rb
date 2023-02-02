881c5d4dfb4698ba6891275e887e3b3c8556edc1
mansakondo <mansakondo22@gmail.com>
2023-02-02 12:42:55 +0100

    Fix migration

--- db/migrate/20210922171917_create_marc_records.rb
+++ db/migrate/20210922171917_create_marc_records.rb
@@ -1,6 +1,6 @@
 class CreateMARCRecords < ActiveRecord::Migration[7.0]
   def change
-    create_table :marc_records do |t|
+    create_table :catalog_marc_records do |t|
       t.string :leader
       t.jsonb :fields
 
